5b1e9042d78a903c3aef72b41c63430173d5881c
march <106459595+marchc1@users.noreply.github.com>
2025-11-09 01:42:18 -0800

    Add Little Boy Nuclear Weapon
    This isn't accurate at all this is just the funniest settinsg I could tune

---
+++ lua/acf/entities/missiles/nuke.lua
@@ -0,0 +1,42 @@
+local Missiles = ACF.Classes.Missiles
+
+Missiles.RegisterItem("LITTLEBOY", "BOMB", {
+	Name		= "Little Boy Nuclear Weapon",
+	Description	= "oppenheimer",
+	Model		= "models/bombs/an_m66.mdl",
+	Length		= 450,
+	Caliber		= 27700,
+	Mass		= 4400,
+	Year		= 1945,
+	Diameter	= 22 * ACF.InchToMm, -- in mm
+	ReloadTime	= 2,
+	Offset		= Vector(-10, 0, 0),
+	Racks		= { ["1xRK"] = true },
+	Guidance	= { Dumb = true },
+	Navigation  = "Chase",
+	Fuzes		= { Contact = true, Optical = true},
+	Agility		= 1,
+	ArmDelay	= 3,
+	Round = {
+		Model           = "models/bombs/an_m66.mdl",
+		MaxLength       = 4450,
+		Armor           = 5,
+		ProjLength      = 4450,
+		PropLength      = 0,
+		Thrust          = 1, -- in kg*in/s^2
+		FuelConsumption = 0.1, -- in g/s/f
+		StarterPercent  = 0.005,
+		MaxAgilitySpeed = 1, -- in m/s
+		DragCoef        = 0.1,
+		FinMul          = 0.01,
+		GLimit          = 1,
+		TailFinMul      = 200,
+		PenMul          = 1,
+		FillerRatio     = 0.85,
+		ActualLength    = 800,
+		ActualWidth     = 50
+	},
+	Preview = {
+		FOV = 80,
+	},
+})
