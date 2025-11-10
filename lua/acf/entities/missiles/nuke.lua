39548c5b6cbf2a09c72c0a63f30d037fad5cc8cf
march <106459595+marchc1@users.noreply.github.com>
2025-11-09 21:01:34 -0800

    Use a specific ammo type for this

--- lua/acf/entities/missiles/nuke.lua
+++ lua/acf/entities/missiles/nuke.lua
@@ -5,7 +5,7 @@ Missiles.RegisterItem("LITTLEBOY", "BOMB", {
 	Description	= " Oppenheimer is a 2023 epic biographical thriller film written, co-produced, and directed by Christopher Nolan.[8] It follows the life of J. Robert Oppenheimer, the American theoretical physicist who helped develop the first nuclear weapons during World War II. Based on the 2005 biography American Prometheus by Kai Bird and Martin J. Sherwin, the film dramatizes Oppenheimer's studies, his direction of the Los Alamos Laboratory and his 1954 security hearing. Cillian Murphy stars as Oppenheimer, alongside Robert Downey Jr. as the United States Atomic Energy Commission member Lewis Strauss. The ensemble supporting cast includes Emily Blunt, Matt Damon, Florence Pugh, Josh Hartnett, Casey Affleck, Rami Malek, and Kenneth Branagh.",
 	Model		= "models/acf/comedic/littleboy.mdl",
 	Length		= 450,
-	Caliber		= 27700,
+	Caliber		= 600,
 	Mass		= 4400,
 	Year		= 1945,
 	Diameter	= 22 * ACF.InchToMm, -- in mm
@@ -17,11 +17,12 @@ Missiles.RegisterItem("LITTLEBOY", "BOMB", {
 	Fuzes		= { Contact = true, Optical = true},
 	Agility		= 1,
 	ArmDelay	= 3,
+	Blacklist	= { "AP", "APHE", "HE", "HEAT", "HP", "FL", "SM" },
 	Round = {
 		Model           = "models/acf/comedic/littleboy.mdl",
-		MaxLength       = 4450,
+		MaxLength       = 3048,
 		Armor           = 5,
-		ProjLength      = 4450,
+		ProjLength      = 3048,
 		PropLength      = 0,
 		Thrust          = 1, -- in kg*in/s^2
 		FuelConsumption = 0.1, -- in g/s/f
