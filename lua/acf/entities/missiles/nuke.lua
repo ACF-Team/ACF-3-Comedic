52427f664f92353b6c65eb10591672105a497520
march <106459595+marchc1@users.noreply.github.com>
2025-11-09 20:36:48 -0800

    Nothing really works for this description tbh

--- lua/acf/entities/missiles/nuke.lua
+++ lua/acf/entities/missiles/nuke.lua
@@ -2,7 +2,7 @@ local Missiles = ACF.Classes.Missiles
 
 Missiles.RegisterItem("LITTLEBOY", "BOMB", {
 	Name		= "Little Boy Nuclear Weapon",
-	Description	= "The worlds most expensive rice cooker, courtesy of the Los Alamos Laboratory",
+	Description	= " Oppenheimer is a 2023 epic biographical thriller film written, co-produced, and directed by Christopher Nolan.[8] It follows the life of J. Robert Oppenheimer, the American theoretical physicist who helped develop the first nuclear weapons during World War II. Based on the 2005 biography American Prometheus by Kai Bird and Martin J. Sherwin, the film dramatizes Oppenheimer's studies, his direction of the Los Alamos Laboratory and his 1954 security hearing. Cillian Murphy stars as Oppenheimer, alongside Robert Downey Jr. as the United States Atomic Energy Commission member Lewis Strauss. The ensemble supporting cast includes Emily Blunt, Matt Damon, Florence Pugh, Josh Hartnett, Casey Affleck, Rami Malek, and Kenneth Branagh.",
 	Model		= "models/acf/comedic/littleboy.mdl",
 	Length		= 450,
 	Caliber		= 27700,
